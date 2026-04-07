b4685752f95b1b0859fc72e38f1084bf992bebb6
Dulor Kisku <dulorai@Dulors-MacBook-Air-2.local>
2026-04-07 11:28:50 +0530

    chore: optimize appwrite-init.js and fix PlanCard syntax error

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'jest-expo',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|lucide-react-native)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|lucide-react-native|react-native-appwrite)',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   collectCoverage: true,
@@ -11,5 +11,14 @@ module.exports = {
     '!**/node_modules/**',
     '!**/babel.config.js',
     '!**/jest.setup.js',
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/appwrite-init.js',
+    '<rootDir>/test-.*\\.js',
+    '<rootDir>/appwrite/',
+    '<rootDir>/diagnose-appwrite.js',
+    '<rootDir>/get-executions.js',
+    '<rootDir>/appwrite-config-setup.js'
   ],
 };
