ebf45aa31b39379daf47ca75c11b7d45976fe19d
Dulor Kisku <dulorai@Dulors-MacBook-Air-2.local>
2026-04-19 00:54:52 +0530

    feat: template-only flow, free limit 3 stories/month

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,18 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
+// Get the default Expo Metro configuration.
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = config;
+// Export the config, spreading the defaults. This ensures only supported options are used.
+module.exports = {
+  ...defaultConfig,
+  // If you need to customize resolver or transformer, you can do so here.
+  // resolver: {
+  //   ...defaultConfig.resolver,
+  //   // custom settings
+  // },
+  // transformer: {
+  //   ...defaultConfig.transformer,
+  //   // custom settings
+  // },
+};
