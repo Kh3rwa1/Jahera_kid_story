cb0843620647944fabf8346000c9fcb0964e6227
Dulor Kisku <founder@tumdah.com>
2026-04-20 15:34:54 +0530

    ci: disable collectCoverage, pin test job to Node 20

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|lucide-react-native|react-native-appwrite)',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: [
     '**/*.{ts,tsx}',
     '!**/node_modules/**',
